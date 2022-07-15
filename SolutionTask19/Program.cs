3e26ef70e7e76a05bf97cfd07f7fcc83c93c8802
Ruslan <ruslan.galikeeff@example.com>
2022-07-15 21:32:36 +0300

    SolutionTask19

---
+++ SolutionTask19/Program.cs
@@ -0,0 +1,31 @@
+﻿int coordXPointA;
+int coordXPointA;
+int coordXPointB;
+int coordXPointB;
+void readDataOfPoint()
+{
+Console.WrineLine("Введите координаты Х в точке А");
+int coordXPointA = int.Parse(Console.readLine());
+Console.WrineLine("Введите координаты Y в точке А");
+int coordXPointA = int.Parse(Console.readLine());
+
+Console.WrineLine("Введите координаты Y в точке B");
+int coordXPointB = int.Parse(Console.readLine());
+Console.WrineLine("Введите координаты Y в точке B");
+int coordXPointB = int.Parse(Console.readLine());
+
+}
+
+//Вычисляет расстояние между точками А и В
+void conculateLengthAB()
+{
+    int LeghtAB = Math.Sqrt(Math.Pow((coordXPointA-coordXPointB),2)+ Math.Pow((coordXPointA-coordXPointB),2));
+}
+
+
+
+readDataOfPoint();
+conculateLengthAB();
+
+
+Console.Writeline(LengtAB);
