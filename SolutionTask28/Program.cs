c97bbf7ef74324c4dfeb08c2a964d80e802e7f70
Ruslan <ruslan.galikeeff@example.com>
2022-08-02 22:31:04 +0300

    SolutionTask28

---
+++ SolutionTask28/Program.cs
@@ -0,0 +1,50 @@
+﻿//Console.Write("Введите число: ");
+//введите число
+//string? inputLine = Console.ReadLine();
+// вещественный тип
+//int inputNum = int.Parse(inputLine);
+//выдаем и передаем число
+//Console.WriteLine(sumNums(inputNum));
+// выдающий и принимающий метод
+//int sumNums(int num)
+//{
+    //int sum = 1;
+    //for (int i = 1; i <= inputNum; i++)
+    //{
+      //  sum *= i;
+    //}
+  //  return sum;
+//}
+
+//Рекурентное решение
+Console.Write("Введите число: ");
+string? inputLine = Console.ReadLine();
+int inputNum = int.Parse(inputLine);
+int t;
+
+t = Environment.TickCount;
+Console.WriteLine(sumNums(inputNum));
+Console.Wrineline("Simple time: {0} ms", Environment.TickCount - t);
+
+t = Environment.TickCount;
+Console.Wrineline("Simple time: {0} ms", Environment.TickCount - t);
+Console.WriteLine(mulRec(inputNum));
+
+int sumNums(int num)
+{
+    int sum = 1;
+    for (int i = 1; i <= inputNum; i++)
+    {
+        sum *= i;
+    }
+    return sum;
+}
+
+int mulRec(int num) 
+{
+    if (num == 1) {
+        return 1;
+    } else {
+        return num * mulRec(num - 1);
+    }
+}
