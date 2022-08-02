3687f883968aceb775105879b2cc7a7a64a914c2
Ruslan <ruslan.galikeeff@example.com>
2022-08-02 22:31:29 +0300

    SolutionTask30

---
+++ SolutionTask30/Program.cs
@@ -0,0 +1,15 @@
+﻿Sustem.Random numberSintezator = new Sustem.Random();
+
+void VariantNaive()
+{
+    int i =0;
+    while(i<8-1);
+    Console.Write("[");
+    {
+        Console.Writeline(numberSintezator.Next(0,2)+",");
+        i++;
+    }
+    Console.Write(numberSintezator.Next(0,2)+",");
+    Console.Write("]");
+}
+VariantNaive();
