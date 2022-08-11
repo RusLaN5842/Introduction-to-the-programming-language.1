0faff933efbcd5e03831a62bedbec3e5ccdc3ff0
Ruslan <ruslan.galikeeff@example.com>
2022-08-11 22:09:02 +0300

    SolytionTask18

---
+++ SolytionTask18/Program.cs
@@ -0,0 +1,23 @@
+﻿string? inputline = Console.ReadLine();
+
+void printAnswer(int Number)
+
+{
+
+
+    if(Number==1) Console.WriteLine("Допустимо: x>0, y>0");
+    if(Number==2) Console.WriteLine("Допустимо: x<0, y>0");
+    if(Number==3) Console.WriteLine("Допустимо: x<0, y<0");
+    if(Number==4) Console.WriteLine("Допустимо: x>0, y<0");
+
+}
+
+string? inputLine = Console.ReadLine();
+
+if(inputLine !=null)
+{
+      
+      int inputNumber = int.Parse(inputline);
+
+printAnswer(inputNumber);
+}
