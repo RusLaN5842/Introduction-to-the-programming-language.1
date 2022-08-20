fdf789310be67e3f2936564d7948315dbf97778d
Ruslan <ruslan.galikeeff@example.com>
2022-08-20 16:44:18 +0300

    Example

---
+++ Example013_Recursion_Algoritm/Program.cs
@@ -0,0 +1,50 @@
+﻿//string[,] table = new string[2,5];
+    //Strig.Empty
+    //table [0,0] table[0,1] table[0,2] table [0,4]
+    //table[1,0] table[1,1] table[1,2] table [,4]
+//table[1,2]="Слово";
+
+//for (int rows = 0; rows < length; rows++)
+//{
+    //for (int columns = 0; columns < length; columns++)
+    //{
+    //    Console.WriteLine($"-{table[rows, colums]}-");
+
+  //  }
+//}
+    // Вывод матрицы чисел или прямоугольная таблица чисел 3 строки и 4 столбца
+
+
+
+
+void PrintArray(int[,]matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+{
+        for (int j = 0; j < matr.GetLength(1); j++)
+    {
+        Console.Write($"{matr[i,j]} ");
+    }
+    Console.WriteLine();
+}
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,10); // [1; 10]
+        }
+    }
+}
+
+int[,] matrix = new int[3,4];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+
+//int[,] matrix = new int [3,4];
+//PrintArray(matrix);
---
+++ Example_string_with_numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿// Собрать строку с числами от а до b,a<=b
+string NumbersFor(int a, int b)
+{
+    string result = String.Empty;
+    for (int i = a; i <= b; i++)
+    {
+    result += $"{i} ";
+    }
+
+return result;
+}
+string NumbersRes(int a, int b)
+{
+    if (a <= b) return $"{a} " + NumbersRes (a + 1, b);
+    else return String.Empty;
+}
+
+Console.WriteLine(NumbersFor(1, 10)); //1 2 3 4 5 6 7 8 9 10
+Console.WriteLine(NumbersRes(1, 10)); // 1 2 3 4 5 6 7 8 9 10
+
